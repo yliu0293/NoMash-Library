b7287f783e80450605ffb60f8f40c396c591db7d
Yugong Liu <yliu0293@student.monash.edu>
2024-10-07 09:42:21 +1100

    w11-3

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -11,7 +11,7 @@
 
 <script>
 import { ref, onMounted } from 'vue';
-import db from '../firebase/init.js';
+import db from '@/Firebase/init';
 import { collection, query, where, getDocs } from 'firebase/firestore';
 
 export default {
