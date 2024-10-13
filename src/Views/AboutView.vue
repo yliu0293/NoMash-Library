1c0da0481da4306cfb9d9f29437b40b8f0e0b6d5
Yugong Liu <yliu0293@student.monash.edu>
2024-10-14 09:22:36 +1100

    add router history base_URL

--- src/Views/AboutView.vue
+++ src/Views/AboutView.vue
@@ -2,6 +2,7 @@
   <div class="about">
     <h1>About Our Library</h1>
     <h1>About Our Library</h1>
+    <h1>About Our Library</h1>
     <p>Welcome to our digital library! We're dedicated to providing a vast collection of books and resources to our community.</p>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -80,7 +80,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
