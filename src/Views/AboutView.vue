f1a5e180355c534cb3bbc042143f5af75ea20e5d
Yugong Liu <yliu0293@student.monash.edu>
2024-10-14 09:17:55 +1100

    small change

--- src/Views/AboutView.vue
+++ src/Views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>About Our Library</h1>
+    <h1>About Our Library</h1>
     <p>Welcome to our digital library! We're dedicated to providing a vast collection of books and resources to our community.</p>
   </div>
 </template>
